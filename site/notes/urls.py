8f9bbd196fab35f9b7962becba40a279ebc3bf34
Kirill Shmakov <menato@yandex-team.ru>
2013-07-17 09:20:14 +0400

    notes work

--- site/notes/urls.py
+++ site/notes/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from notes.views import metaphysics, mp_note
-from notes.views import technology
+from notes.views import technology, th_note
 
 def get():
     return patterns('',
         url(r'^notes/mp/$', metaphysics),
         url(r'^notes/mp/(?P<num>\d+)/$', mp_note),
-        url(r'^notes/th/$', technology)
+        url(r'^notes/th/$', technology),
+        url(r'^notes/th/(?P<num>\d+)/$', th_note)
     )
