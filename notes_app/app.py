76c2deab73a5c54ac042aacef5f89c7c0c92378a
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-03-12 13:47:41 +0300

    App for notes.

---
+++ notes_app/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hi" # render_template('welcome.html', static_url='//kishmakov.ru/static/')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
