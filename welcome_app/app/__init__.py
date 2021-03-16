23c2267ff2b8acb1be96db1994691560b6dba501
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-03-16 11:52:54 +0300

    Make notes IDE friendly.

--- welcome_app/app/__init__.py
+++ welcome_app/app/__init__.py
@@ -4,9 +4,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template('welcome.html', static_url='//kishmakov.ru/static/')
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
