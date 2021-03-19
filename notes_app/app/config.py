b7bf47a24250b746ca346ba423592a0f9e4025ba
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-03-20 00:22:00 +0300

    Add link to edit form on Github for notes.

--- notes_app/app/config.py
+++ notes_app/app/config.py
@@ -1,6 +1,7 @@
 class Base(object):
     HOME_URL = "http://kishmakov.ru/"
     STATIC_URL = "http://kishmakov.ru/static/"
+    GITHUB_EDIT_URL = "https://github.com/kishmakov/myweb/edit/master/notes_app/notes/"
 
 
 class Dev(Base):
