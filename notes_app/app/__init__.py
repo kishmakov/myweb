8518f2dfb6d3c4cb670047621735a5c3ebeab098
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-03-19 19:52:35 +0300

    Show tags in list view.

--- notes_app/app/__init__.py
+++ notes_app/app/__init__.py
@@ -12,18 +12,21 @@ def create_app():
         app.config.from_object("config.Prod")
 
     sorted_notes = sorted(notes_records, key=lambda record: record.id, reverse=True)
+    tags = sorted(list(set([tag for record in notes_records for tag in record.tags])))
 
     @app.route("/")
-    def index():
+    def welcome():
         return redirect("/list")
 
     @app.route("/list")
     def list_all():
-        return render_template("list_all.html", notes=sorted_notes  )
+        return render_template("list_all.html", notes=sorted_notes, tags=tags)
 
     @app.route("/list/<tag>")
     def list_tagged(tag):
-        return render_template("list_tagged.html", notes=sorted_notes)
+        tagged_notes = filter(lambda n: tag in n.tags, sorted_notes)
+        other_tags = filter(lambda t: t != tag, tags)
+        return render_template("list_tagged.html", tag=tag, notes=tagged_notes, tags=other_tags)
 
     @app.route("/n/<note_id>")
     def note(note_id):
