fd975a6e385f5eea6f042e137e6f65b9b9705e2d
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-03-19 20:21:47 +0300

    Improve reload scripts.

--- configs/reload_notes_app.sh
+++ configs/reload_notes_app.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-docker pull kishmakov/notes_app:latest
+if [ -z "$1" ]; then
+  docker pull kishmakov/notes_app:latest
+elif [ "$1" == "local" ]; then
+  docker build -t kishmakov/notes_app:latest ../notes_app/
+fi
+
 docker stop notes_cont
 docker rm notes_cont
 docker run -d -p 5002:5003 --name notes_cont kishmakov/notes_app:latest
--- configs/reload_welocome_app.sh
+++ configs/reload_welocome_app.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-docker pull kishmakov/welcome_app:latest
+if [ -z "$1" ]; then
+  docker pull kishmakov/welcome_app:latest
+elif [ "$1" == "local" ]; then
+  docker build -t kishmakov/welcome_app:latest ../welcome_app/
+fi
+
 docker stop welcome_cont
 docker rm welcome_cont
 docker run -d -p 5000:5001 --name welcome_cont kishmakov/welcome_app:latest
